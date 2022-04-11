1aacfb6f4aec7b967fb541ab1277eacd2c6aa453
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-10 20:35:31 -0700

    Adding Codewars solution for April 10th

---
+++ solution_0005/solution_0005.js
@@ -0,0 +1,7 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x) {
+    return x.split(' ').join('')
+}
+//test cases
+noSpace('8 j 8 mBliB9 g imJ Bbb99 B')
+noSpace('aa bbbb cccccc ddddd')
