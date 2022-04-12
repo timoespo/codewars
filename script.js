993ccef7c53acbed5395d2ecdddd5d5f271b77c7
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-12 11:21:27 -0700

    Adding 2nd Codewars for April 12th to main.

--- script.js
+++ script.js
@@ -86,3 +86,27 @@ class SmallestIntegerFinder {
 //test cases 
 findSmallestInt([78,56,232,12,8]) // 8
 findSmallestInt([1,56,232,12,8]) // 1
+
+
+//April 12th: Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+function countSheeps(arrayOfSheep) {
+    let totalSheep = 0
+    for (let i = 0 ; i < arrayOfSheep.length ; i++) {
+        if (arrayOfSheep[i] === true) {
+            totalSheep++
+        }
+    } return totalSheep
+}
+
+//test cases
+let array1 = [true, true, true, false, true, true, true, true, false, true, false, true]
+countSheeps(array1) // 9 Sheeps in total
+
+//Solution I thought was clever on Codewars
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;   // first time seeing the filter method being used on Boolean
+  }
+
+
+
+
