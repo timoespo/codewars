3d9d0889ba060528cd055a6d421bbaaee7480096
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-13 10:45:24 -0700

    Adding third challenge for April 13th to Main

--- script.js
+++ script.js
@@ -133,3 +133,14 @@ century(1705) // 18
 century(1900) // 19
 century(1601) //17
 
+//April 13th: Abbreviate a two name word - Write a function to convert a name into initials. This kata strictly takes two words with one space in between them. The output should be two capital letters with a dot separating them.
+function abbrevName(name) {
+    let x = name.split(" ")[1]
+    let y = name.substring(name.indexOf(" ")+ 1)
+    return `${x[0]}.${y[0]}`
+}
+
+//test cases
+abbrevName("Sam Harris") // "S.H"
+abbrevName("Patrick Feenan") //"P.F"
+abbrevName("Evan Cole") // "E.C"
