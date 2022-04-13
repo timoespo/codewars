40250f56fc1dfd0e0711b79772efa553ae5e5c8b
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-13 10:11:25 -0700

    Adding April 13th Challenge to Main

--- script.js
+++ script.js
@@ -108,5 +108,18 @@ function countSheeps(arrayOfSheeps) {
   }
 
 
+//April 13th: Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+function isDivisible(n,x,y){
+    if(n % x === 0 && n % y === 0) {
+        return true 
+    } else {
+        return false
+    }
+}
+
+//test cases
+isDivisible(3,3,4) // false
+isDivisible(12,3,4) // true
+isDivisible(8,3,4) // false
 
 
