89df0276a2aff028494d5156cb8f07786477724d
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-13 10:31:11 -0700

    Adding second challenge for April 13th to main

--- script.js
+++ script.js
@@ -123,3 +123,13 @@ isDivisible(12,3,4) // true
 isDivisible(8,3,4) // false
 
 
+//April 13th: Century from Year: The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+function century(year) {
+    return Math.ceil(year/100) //using Math.ceil will round up
+}
+
+//test cases
+century(1705) // 18
+century(1900) // 19
+century(1601) //17
+
