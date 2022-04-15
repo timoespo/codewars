e33452dc08b685202c3e8a5ef2602e6529e5b420
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-14 19:55:01 -0700

    Adding third Codewars Challenge for April 14th to main

--- script.js
+++ script.js
@@ -173,3 +173,18 @@ stringToNumber("605")//605
 function maps(x) {
     return treasureMap = x.map( n => n * 2)
 }
+
+//test cases
+maps([1,2,3,4])
+maps([2.4,6,8])
+
+
+//April 14th: Clock shows h hours, m minutes and s seconds after midnight. Your task is to write a function which returns the time since midnight in milliseconds.
+function past( h, m, s) {
+    let ms = (h * 3600 * 1000) + (m * 60 * 1000) + (s * 1000)
+    return ms
+}
+
+//test cases 
+past(0, 1, 1) // 61000
+past(1, 1, 1) // 3661000
