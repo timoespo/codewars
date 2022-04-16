8854aa26c4455684a547d6d0a7ee4e6fc5de7975
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-16 10:41:37 -0700

    Adding third Codewars challenge for April 16th to main

--- script.js
+++ script.js
@@ -216,3 +216,15 @@ function greet(name) {
 //test cases 
 greet('Johnny') // 'Hello, my love!
 greet('Jimmy')// 'Hello, Jimmy!
+
+//April 16th: It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script. Return the average of the given array rounded down to its nearest integer.
+function getAverage(marks) {
+    let total = 0
+    for (let i = 0 ; i < marks.length ; i++) {
+     total += marks[i]   
+    } return Math.floor(total / marks.length)
+}
+
+//test cases
+getAverage([2,2,2,2]) // 2
+getAverage([1,2,3,4,5]) // 3
