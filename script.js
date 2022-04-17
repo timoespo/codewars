7e2269ce796a0a17b5499d2ec6038b603c4c624e
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-17 13:02:52 -0700

    Adding fifth Codewars challenge for April 17th to main

--- script.js
+++ script.js
@@ -285,3 +285,13 @@ function monkeyCount(n) {
 //test cases 
 monkeyCount(5) //[1,2,3,4,5]
 monkeyCount(3) // [1,2,3]
+
+
+//April 17th: Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love. Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+function lovefunc(flower1, flower2) {
+    return (flower1%2 !== flower2%2) ? true : false
+}
+
+//test cases
+lovefunc(1,4) // true
+lovefunc(2,2) //false
