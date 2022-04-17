6d717e6e4cf70fa6864bde81ae3c6976ce0feca9
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-17 12:48:13 -0700

    Adding fourth Codewars challenge for April 17th to main

--- script.js
+++ script.js
@@ -273,3 +273,15 @@ function simpleMultiplication(number) {
 
 simpleMultiplication(2) // 16
 simpleMultiplication(1) //9 
+
+//April 17th: You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1. As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+function monkeyCount(n) {
+    let monkeyArr = []
+    for (let i = 1 ; i <=  n ; i++) {
+        monkeyArr.push(i)
+    } return monkeyArr
+}
+
+//test cases 
+monkeyCount(5) //[1,2,3,4,5]
+monkeyCount(3) // [1,2,3]
