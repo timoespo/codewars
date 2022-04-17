6ccf7e7313940692b71425b1a6a6f6d290a24d83
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-17 11:05:53 -0700

    Adding third Codewars challenge for April 17th to main

--- script.js
+++ script.js
@@ -265,3 +265,11 @@ function hero(bullets, dragons) {
 //test cases
 hero(10, 5) // true
 hero(7, 4) // false
+
+//April 17th: This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(number) {
+    return (number%2 === 0) ? number * 8 : number * 9
+}
+
+simpleMultiplication(2) // 16
+simpleMultiplication(1) //9 
