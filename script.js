2037493df4a28e563536e043d10dc8bc9e100e22
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-19 21:56:21 -0700

    Adding Codewars challenge for April 19th to main

--- script.js
+++ script.js
@@ -305,3 +305,17 @@ function removeExclamationMarks(s) {
 
 //test cases 
 RemoveExclamationMarks("Hello World!") // Hello World
+
+//April 19th: Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+const corrections = {
+    '5' : 'S',
+    '0' : 'O',
+    '1' : 'I',
+};
+const correct = string => {
+    string.replace(/[501]/g, character => corrections[character])
+}
+
+// test cases
+correct('L0ND0N')// "LONDON"
+correct('DUBL1N') // "DUBLIN"
