30864924a710d11b6a968390f93d690be3d1343c
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-20 18:16:46 -0700

    Adding second Codewars challenge for April 20th to main

--- script.js
+++ script.js
@@ -328,3 +328,19 @@ function reverseWords(str){
 //test cases 
 reverseWords("hello world!") // world! hello
 reverseWords("yoda doesn't speak like this")// this like speak doesn't yoda
+
+//April 20th: n this challenge, you are asked to square every digit of a number and concatenate them.
+function squareDigits(num) {
+    let string = num.toString();
+    let results = []
+    for (let i = 0; i < string.length ; i++) {
+        results[i] = string[i] ** 2
+    }
+    return Number(results.join(''))
+}
+
+//test cases 
+squareDigits(3212) // 9414
+squareDigits(2112) // 4114
+
+
