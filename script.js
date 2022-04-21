1b01ad1780219f783fed4c9ce3605507380b6727
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-20 17:59:35 -0700

    Adding Codewars challenge for April 20th to main

--- script.js
+++ script.js
@@ -319,3 +319,12 @@ const correct = string => {
 // test cases
 correct('L0ND0N')// "LONDON"
 correct('DUBL1N') // "DUBLIN"
+
+//April 20th: Complete the solution so that it reverses all of the words within the string passed in.
+function reverseWords(str){
+    return str.split(" ").reverse().join(" ");
+}
+
+//test cases 
+reverseWords("hello world!") // world! hello
+reverseWords("yoda doesn't speak like this")// this like speak doesn't yoda
