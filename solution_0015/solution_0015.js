34f19a5c8b4a17dafa611fbae7c64d48767bc84d
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-20 21:43:35 -0700

    Adding Codewars solution for April 20th

---
+++ solution_0015/solution_0015.js
@@ -0,0 +1,8 @@
+//April 14th: Given an array of integers, return a new array with each value doubled.
+function maps(x) {
+    return treasureMap = x.map( n => n * 2)
+}
+
+//test cases
+maps([1,2,3,4])
+maps([2.4,6,8])
