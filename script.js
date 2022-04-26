5822d9caa1fbc95c9f8863b54477cca24d15b5a6
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-25 20:42:11 -0700

    Adding second Codewars challenge for April 25th

--- script.js
+++ script.js
@@ -360,3 +360,13 @@ function disemvowel(str) {
 function disemvowel(str) {
     return str.replace(/[aeiou]/gi, ''); 
 }
+
+//April 25th: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+function highAndLow(numbers) {
+    numbers = number.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`
+}
+
+// test cases
+highAndLow('1 2 3 4 5')// '5 1'
+highAndLow('1 2 -3 4 5') // '5 -3'
