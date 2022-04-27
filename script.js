d87084e1de01db877e50bc937904221c1daaa92c
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-26 21:51:30 -0700

    Adding Codewars challenge for April 26th to main

--- script.js
+++ script.js
@@ -384,3 +384,18 @@ function descendingOrder(n) {
 //test cases
 descendingOrder(42145) // 54421
 descendingOrder(145263) // 654321
+
+
+//April 26th: Mumbling - This time no story, no theory. 
+function accum(str)  {
+    let letters = str.split('');
+    let result = []
+    for (let i = 0; i < letters.length ; i++) {
+        result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+    }
+    return result.join('-');
+}
+
+//test cases 
+accum('abcd') // 'A-Bb-Ccc-Dddd
+accum('RqaEzty') // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'
