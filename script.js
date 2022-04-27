74a9b887ef314a22a46b9a49fb4f2f7d17f9e76a
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-26 22:07:17 -0700

    Adding second Codewars challenge for April 26th to main

--- script.js
+++ script.js
@@ -399,3 +399,28 @@ function accum(str)  {
 //test cases 
 accum('abcd') // 'A-Bb-Ccc-Dddd
 accum('RqaEzty') // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'
+
+
+//April 26th: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+function getMiddle(s) {
+    let position;
+    let length;
+
+    if(s.length % 2 === 1) {
+        position = s.length  / 2;
+        length = 1
+    } else {
+        position = s.length / 2 - 1
+        length = 2
+    }
+    return s.substring(position, position + length)
+}
+
+//Another solution 
+function getMiddle(s) {
+    return s.substr(Math.ceil(s.length / 2- 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+//test cases 
+getMiddle('test') // 'es'
+getMiddle('testing') // 't'
