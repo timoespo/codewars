489eab79eaf3ebbbe92351c28c157ed4e5f7bc25
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-04-26 22:17:09 -0700

    Adding third Codewars challege for April 26th to main

--- script.js
+++ script.js
@@ -424,3 +424,14 @@ function getMiddle(s) {
 //test cases 
 getMiddle('test') // 'es'
 getMiddle('testing') // 't'
+
+
+//April 26th: Check if a number is  a perfect square
+var isSquare = function(n) {
+    return n > -1 && Math.sqrt(n) % 1 === 0; 
+}
+
+//test cases 
+isSquare(-1) //false 
+isSquare(25) // true
+isSquare(3) //false
