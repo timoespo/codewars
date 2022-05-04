71de521861e56c56ca68f0808bb3b16b90e57a1a
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-03 21:34:06 -0700

    Adding fourth Codewars challenge for May 3rd to main

--- script.js
+++ script.js
@@ -528,3 +528,13 @@ function bonusTime(salary, bonus) {
 
 //test cases 
 bonusTime(100000, true)// £1000000
+
+
+//May 3rd: You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. 
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return (fuelLeft * mpg) >= distanceToPump ? true : false
+}
+
+//test cases 
+zeroFuel(50,25,2) // true
+zeroFuel(100,50,1) // false
