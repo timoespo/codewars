431f8d1eb5b539c4a5fb281d9891d70139a2921d
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-04 21:24:41 -0700

    Adding Codewar challenge for May 4th to main

--- script.js
+++ script.js
@@ -538,3 +538,36 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //test cases 
 zeroFuel(50,25,2) // true
 zeroFuel(100,50,1) // false
+
+
+//May 4th: Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+var max = function(list){
+    let maximum = list[0]
+    for (let i = 0; i < list.length; i++){
+        if(list[i] > maximum) {
+            maximum = list[i]
+        }
+    } return maximum
+}
+var min = function(list){
+    let minimum = list[0]
+    for (let i = 0; i < list.length; i++){
+        if(list[i] < minimum) {
+            minimum = list[i]
+        }
+    } return minimum
+}
+
+//other solution
+let min = (list) => Math.min(...list);
+let max = (list) => Math.max(...list);
+
+//using sorting 
+var min = function(list) {
+    list.sort((a,b) => (a-b));
+    return list[0]
+}
+var max = function(list){
+    list.sort((a,b) => (b-a));
+    return list[0]
+}
