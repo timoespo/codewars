04f665989393ed4e1bca932e7f5739b326fef93a
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-04 21:43:02 -0700

    Adding second Codewars challenge for May 4th to main

--- script.js
+++ script.js
@@ -571,3 +571,19 @@ var max = function(list){
     list.sort((a,b) => (b-a));
     return list[0]
 }
+
+
+//May 4th: Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ). The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+function sumArray(array){
+    if (array == null) {
+        return 0
+    } else if (array.length < 2) {
+        return 0
+    } else {
+        array = array.sort(function(a,b) {return a - b})
+        let total = 0
+        for(let i = 0 ; i < array.length ; i++) {
+            total += array[i]
+     } return total  
+    } 
+}
