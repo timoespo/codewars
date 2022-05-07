5aab222e2cc82a44c060db4b085860ad79ffdf3d
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-06 21:48:14 -0700

    Adding Codewars Challege for May 6th to main

--- script.js
+++ script.js
@@ -587,3 +587,18 @@ function sumArray(array){
      } return total  
     } 
 }
+
+
+//May 6th: Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+function points(games) {
+    let total = 0 
+    for (let i = 0 ; i < games.length ; i++) {
+        if (games[i][0] > games[i][2]){
+            total += 3
+        } else if (games[i][0] === games[i][2]){
+            total += 1
+        } else {
+            total += 0
+        }
+    } return total
+}
