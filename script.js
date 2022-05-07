954a84c9ad3ae77d997523208e117185160b12dc
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-07 08:40:32 -0700

    Adding second Codewars challenge for May 7th to main

--- script.js
+++ script.js
@@ -620,3 +620,13 @@ function stringToArray(string) {
 }   
 //test cases
 stringToArray("Robin Hood") // ['Robin', 'Hood']
+
+
+//May 7th: Sentence Smash - Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+function smash(words) {
+    return words.join(' ')
+}
+
+//test cases 
+smash([]) // ""
+smash(['this', 'is', 'a', 'really', 'long', 'sentence']) //'this is a really long sentence'
