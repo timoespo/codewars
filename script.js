ec14de6eaa75b725ec82e64975a33ebbfaf61d11
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-07 08:31:22 -0700

    Adding Codewars challenge for May 7th to main

--- script.js
+++ script.js
@@ -612,3 +612,11 @@ let countSheep = function(num) {
     }
     return result
 }
+
+
+//May 7th: Convert a string into an array - Write a function to split a string and convert it into an array of words.
+function stringToArray(string) {
+    return string.split(' ')
+}   
+//test cases
+stringToArray("Robin Hood") // ['Robin', 'Hood']
