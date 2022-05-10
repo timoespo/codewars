2bb9cd2bbf4a87e65f54b1961254c8449524a712
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-09 18:01:52 -0700

    Adding Codewars solution for May 9th

---
+++ solution_0034/solution_0034.js
@@ -0,0 +1,12 @@
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+function descendingOrder(n) {
+    return parseInt(String(n).split('').sort().reverse().join(''))
+}
+//another solution
+function descendingOrder(n) {
+    return +(n + '').split('').sort(function(a,b) {return b -a }).join('')
+} 
+
+//test cases
+descendingOrder(42145) // 54421
+descendingOrder(145263) // 654321
