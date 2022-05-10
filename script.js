7cf8842785ebfb9ec09b00f79768ea940f95ca6e
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-09 18:48:49 -0700

    Adding Codewars challenge for May 9th to main

--- script.js
+++ script.js
@@ -630,3 +630,12 @@ function smash(words) {
 //test cases 
 smash([]) // ""
 smash(['this', 'is', 'a', 'really', 'long', 'sentence']) //'this is a really long sentence'
+
+//May 8th: Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number.
+function sumMix(x) {
+    return x.reduce((x,y) => Number(x) + Number(y))
+}
+
+//test cases 
+sumMix([9,3,'7','3'])// 22
+sumMix(['5','0',9,3,2,1,'9',6,7]) //42
