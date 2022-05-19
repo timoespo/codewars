7e474a08a9f75634f9b6e2b4e33750a7ae3b62e4
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-05-18 19:43:32 -0700

    Adding Codewars solution for May 18th

---
+++ solution_0043/solution_0043.js
@@ -0,0 +1,7 @@
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money? Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean. If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+function bonusTime(salary, bonus) {
+    return bonus === true ? `\u00A3${salary * 10}` : `\u00A3${salary}`
+}
+
+//test cases 
+bonusTime(100000, true)// £1000000
