142f8695f6f65c7e91f0f17177aeb99fb144609b
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-06-09 17:11:59 -0700

    Adding Codewars challenge for June 9th

---
+++ solution_0065/solution_0065.js
@@ -0,0 +1,15 @@
+//Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+String.prototype.toAlternatingCase = function() {
+    return this.split("").map( a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
+}
+
+//test cases
+"hello world".toAlternatingCase() // "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() // "hello world"
+"hello WORLD".toAlternatingCase() // "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() // "hEllO wOrld"
+"12345".toAlternatingCase() // "12345"
+"1a2b3c4d5e".toAlternatingCase() // "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() // "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+"Hello World".toAlternatingCase().toAlternatingCase() // "Hello World"
