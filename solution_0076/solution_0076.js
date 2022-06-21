57abf20d566bec9e5cfbb487c53a058a8da8832e
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-06-20 18:24:32 -0700

    Adding Codewars solution for June 20th

---
+++ solution_0076/solution_0076.js
@@ -0,0 +1,10 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
+
+console.log(sayHello('Mr. Spock'), 'Hello, Mr. Spock')
+console.log(sayHello('Captain Kirk'), 'Hello, Captain Kirk')
+console.log(sayHello('lieutenant Uhura'), 'Hello, lieutenant Uhura')
+console.log(sayHello('Dr. McCoy'), 'Hello, Dr. McCoy')
