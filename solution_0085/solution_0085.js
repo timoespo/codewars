0127525f190b7903cb3365f89e13bc44856571c4
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-06-29 16:49:51 -0700

    Adding Codewars Solution for June 29th

---
+++ solution_0085/solution_0085.js
@@ -0,0 +1,13 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+function DNAStrand(dna) {
+  let compDNA = { A: 'T', T: 'A', C: 'G', G: 'C' }
+  return dna.replace(/./g, (e) => compDNA[e])
+}
+
+console.log(DNAStrand('AAAA'), 'TTTT')
+console.log(DNAStrand('ATTGC'), 'TAACG')
