0ee0f3575e3aec2a296d8d0cd4cc4bc57fb4c03f
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-08-11 22:47:13 -0700

    Adding Codewars Solution for August 11th

---
+++ solution_0125/solution_0125.js
@@ -0,0 +1,12 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+function smallEnough(a, limit) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > limit) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(smallEnough([66, 101], 200), true)
