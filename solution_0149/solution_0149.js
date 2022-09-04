6dbde27f9ef6e56859a10d72817597ba3e31854a
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-09-04 12:02:36 -0700

    Adding Codewars Solution for September 4th

---
+++ solution_0149/solution_0149.js
@@ -0,0 +1,19 @@
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+
+function predictAge(...ages) {
+  let total = 0
+  for (i in ages) {
+    total += ages[i] ** 2
+  }
+  return (Math.sqrt(total) / 2) | 0
+}
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45), 86)
