14d8235964adb73008fdcfeab8de87d0068c5d50
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-09-07 12:55:31 -0700

    Adding Codewars Solution for September 7th

---
+++ solution_0152/solution_0152.js
@@ -0,0 +1,10 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds(values) {
+  return values.filter((num) => num % 2 === 0)
+}
+
+console.log(noOdds([0, 1]), [0])
+console.log(noOdds([0, 1, 2, 3]), [0, 2])
