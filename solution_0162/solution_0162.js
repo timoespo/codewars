903a111103eaa0df4000858b1f652c8159fb99d2
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-09-17 14:01:21 -0700

    Adding Codewars Solution for September 17th

---
+++ solution_0162/solution_0162.js
@@ -0,0 +1,48 @@
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+// Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+// vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+// Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//  hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// or printed:
+
+// vertical mirror   |horizontal mirror
+// abcd --> dcba     |abcd --> mnop
+// efgh     hgfe     |efgh     ijkl
+// ijkl     lkji     |ijkl     efgh
+// mnop     ponm     |mnop     abcd
+// Task:
+// Write these two functions
+// and
+
+// high-order function oper(fct, s) where
+
+// fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+// s = "abcd\nefgh\nijkl\nmnop"
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+// Note:
+// The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".
+
+function vertMirror(s) {
+  return s.map((s) => [...s].reverse().join(''))
+}
+
+function horMirror(s) {
+  return s.reverse()
+}
+
+function oper(fct, s) {
+  return fct(s.split('\n')).join('\n')
+}
+
+console.log(
+  oper(vertMirror, 'hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu'),
+  'QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw'
+)
