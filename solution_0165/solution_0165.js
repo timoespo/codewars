56976d77aa77a8e3060a4e804341abd6f74e405b
Timothy.Esposito <esposito_timothy@yahoo.com>
2022-09-20 14:29:27 -0700

    Adding Codewars Solution for September 20th

---
+++ solution_0165/solution_0165.js
@@ -0,0 +1,15 @@
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer) {
+  return ('+'.repeat(integer) + '\n').repeat(integer).trim()
+}
+
+console.log(generateShape(8))
+console.log(generateShape(3))
