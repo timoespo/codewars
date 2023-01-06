1c3d3bcd46b00336d5892b8906ed1597bbbd2eb2
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-01-06 13:36:32 -0800

    Adding Codewars Solution for January 6th

---
+++ solution_0262/solution_0262.js
@@ -0,0 +1,19 @@
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+
+// Valid inputs examples:
+// Examples of valid inputs:
+// 1.2.3.4
+// 123.45.67.89
+// Invalid input examples:
+// 1.2.3
+// 1.2.3.4.5
+// 123.456.78.90
+// 123.045.067.089
+
+function isValidIP(str) {
+  return /^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/.test(
+    str
+  )
+}
+console.log(isValidIP('0.0.0.0'), true)
+console.log(isValidIP('abc.def.ghi.jkl'), false)
