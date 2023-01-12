a445333325b00ac77c8c662aec059472ce9184dc
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-01-11 16:23:49 -0800

    Adding Codewars Solution for January 11th

---
+++ solution_0267/solution_0267.js
@@ -0,0 +1,14 @@
+// An Arithmetic Progression is defined as one in which there is a constant difference between the consecutive terms of a given series of numbers. You are provided with consecutive elements of an Arithmetic Progression. There is however one hitch: exactly one term from the original series is missing from the set of numbers which have been given to you. The rest of the given series is the same as the original AP. Find the missing term.
+
+// You have to write a function that receives a list, list size will always be at least 3 numbers. The missing term will never be the first or last one.
+
+// Example
+// findMissing([1, 3, 5, 9, 11]) == 7
+
+function findMissing(l) {
+  return (
+    ((l[0] + l[l.length - 1]) * (l.length + 1)) / 2 - l.reduce((a, b) => a + b)
+  )
+}
+
+console.log(findMissing([1, 3, 4]), 2)
