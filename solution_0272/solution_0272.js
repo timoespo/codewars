6c0b89ded592e43171207285d2dad7d5255ad811
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-01-16 11:12:05 -0800

    Adding Codewars Solution for January 16th

---
+++ solution_0272/solution_0272.js
@@ -0,0 +1,25 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [ 8, 64 ]
+
+function parse(data) {
+  let result = []
+  data.split('').reduce((cur, s) => {
+    if (s === 'i') cur++
+    if (s === 'd') cur--
+    if (s === 's') cur = Math.pow(cur, 2)
+    if (s === 'o') result.push(cur)
+    return cur
+  }, 0)
+  return result
+}
+
+console.log(parse('iiisdoso'), [8, 64])
