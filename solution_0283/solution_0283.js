7ed0cf6802d21a4ea69f644866fd3d6ed227df3a
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-01-27 15:40:55 -0800

    Adding Codewars Solution for January 27th

---
+++ solution_0283/solution_0283.js
@@ -0,0 +1,54 @@
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+// (depend on the language)
+
+// productFib(714) # should return (21, 34, true),
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false),
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true],
+// productFib(800) # should return [34, 55, false],
+// -----
+// productFib(714) # should return {21, 34, 1},
+// productFib(800) # should return {34, 55, 0},
+// -----
+// productFib(714) # should return {21, 34, true},
+// productFib(800) # should return {34, 55, false}
+
+function productFib(prod) {
+  let fib = [0, 1]
+  for (let i = 0; fib[i + 1] < prod; i++) {
+    fib.push(fib[i] + fib[i + 1])
+  }
+  for (let j = 0; j < fib.length; j++) {
+    if (fib[j] * fib[j + 1] === prod) {
+      return [fib[j], fib[j + 1], true]
+    }
+    if (fib[j] * fib[j + 1] !== prod && fib[j] * fib[j + 1] > prod) {
+      return [fib[j], fib[j + 1], false]
+    }
+  }
+}
