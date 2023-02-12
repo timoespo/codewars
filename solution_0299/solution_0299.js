556222756b476a343c04462321c5df4b5cb9d833
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-02-12 12:04:17 -0800

    Adding Codewars Solution for February 12th

---
+++ solution_0299/solution_0299.js
@@ -0,0 +1,15 @@
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+// Note: the subarrays should be filled with 1s
+
+function pyramid(n) {
+  return Array.from({ length: n }, (x, i) => Array(i + 1).fill(1))
+}
+
+console.log(pyramid(0), [])
+console.log(pyramid(1), [1])
+console.log(pyramid(2), [1], [1, 1])
