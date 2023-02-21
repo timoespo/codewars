c49b6c2cf4cc1df8ffa3caf2479e3ad97a5afbfe
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-02-21 08:35:56 -0800

    Adding Codewars Solution for February 21st

---
+++ solution_0308/solution_0308.js
@@ -0,0 +1,9 @@
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+function reverse(str) {
+  return str
+    .trim()
+    .split(' ')
+    .map((v, i) => (i % 2 !== 0 ? v.split('').reverse().join('') : v))
+    .join(' ')
+}
