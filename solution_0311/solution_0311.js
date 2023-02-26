006624a8fa08a2d29f0eb7064250a0a7aae7548d
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-02-26 10:19:58 -0800

    Adding Codewars Solution for February 26th

---
+++ solution_0311/solution_0311.js
@@ -0,0 +1,15 @@
+// In mathematics, Pascal's triangle is a triangular array of the binomial coefficients
+
+// where n denotes a row of the triangle, and k is a position of a term in the row.
+
+// Pascal's Triangle
+
+// You can read Wikipedia article on Pascal's Triangle for more information.
+
+// Task
+// Write a function that, given a depth n, returns n top rows of Pascal's Triangle flattened into a one-dimensional list/array.
+
+// Example:
+// n = 1: [1]
+// n = 2: [1,  1, 1]
+// n = 4: [1,  1, 1,  1, 2, 1,  1, 3, 3, 1]
