b64b9e7664b6c4d5da7338d68391a71b4717e1db
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-03-06 11:04:00 -0800

    Adding Codewars Solution for March 6th

---
+++ solution_0319/solution_0319.js
@@ -0,0 +1,20 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+function remDup(arr) {
+  return arr.map((w) => {
+    return w
+      .split('')
+      .filter((c, i, arr) => {
+        return c !== arr[i - 1]
+      })
+      .join('')
+  })
+}
